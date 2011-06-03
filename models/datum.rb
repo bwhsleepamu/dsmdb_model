6eaaa701ab387ed5b05f483cfbb4922ba2afb4a2
Piotr Mankowski <pmankowski@partners.org>
2011-06-03 17:03:54 -0400

    End of week commit

--- models/datum.rb
+++ models/datum.rb
@@ -1,7 +1,7 @@
 class Datum < ActiveRecord::Base
   set_primary_key self.name.downcase+'_id'
   set_sequence_name 'id_seq'
-  attr_accessible :event_id, :unit_id, :title, :numeric, :char, :description
+  attr_accessible :event_id, :unit_id, :title, :numeric, :char, :description, :timepoint
   
   belongs_to :unit
   belongs_to :event
