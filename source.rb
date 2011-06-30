66eaa41562f9dd792d01003899c40d888353830a
Piotr Mankowski <pmankowski@partners.org>
2011-06-30 16:42:43 -0400

    working of final release

--- source.rb
+++ source.rb
@@ -4,4 +4,16 @@ class Source < ActiveRecord::Base
   attr_accessible :user, :source_type, :reference, :description
   
   has_many :events
+  has_many :data
+  
+  def self.create_source?(attr)
+    unless attr.nil?
+     unless (attr[:source_type].empty? and attr[:source_type].empty? and attr[:description].empty?)
+        return true
+      end
+    end
+    
+    return false
+  end
+
 end
