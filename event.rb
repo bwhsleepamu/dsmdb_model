5367865683df7edba5ea7730e41697cc0940ae83
Piotr Mankowski <pmankowski@partners.org>
2011-07-08 17:19:52 -0400

    finished bulk of updates - ready for data input again

--- event.rb
+++ event.rb
@@ -13,7 +13,169 @@ class Event < ActiveRecord::Base
   
   after_save :save_data
   
+  
+  # add demographic tags
+  def add_demographic_tags
+    add_tags(["demographic", "manual", "merged"])
+  end
+  
+  # add form tags
+  def add_form_tags
+    add_tags(["demographic", "manual", "form"])
+  end
+
+  def add_tags(tag_list)
+    tag_list.each do |tag|
+      event_tags << EventTag.find_or_create_by_tag_name(tag)      
+    end
+  end
+  
+  ## Creators for different types of events ##
+  # return an event object for holding subject demographic data
+  def self.new_subject_demographics(subject)
+    event = Event.new({:name => "demographics"})
+    event.subject = subject
+    event.add_demographic_tags
+    event.add_data_by_event_type
+    event.realtime = Time.now()
+    event.source = Source.new({:user => Authorization.current_user.id, :source_type => "merged", 
+                        :reference => "Merged Forms page for subject #{subject.subject_id}"
+                       })  
+    event
+  end
+  
+  def self.new_form(form_name, subject_id)
+    # maybe make this the accessor and creator?
+
+    CUSTOM_LOGGER.info "Creating form #{form_name} for #{subject_id}"
+    e = Event.where("name = ? AND subject_id = ?", form_name, subject_id).first
+    CUSTOM_LOGGER.info e ? e.event_id : "no other such event found"
+    
+    if e.nil?
+      event = Event.new(:name => form_name)
+      event.subject_id = subject_id
+      event.add_data_by_event_type
+      event.add_form_tags
+      event.source = Source.new({:user => Authorization.current_user.id, :source_type => "physical", 
+                                  :reference => "File Room - folder for subject #{event.subject.subject_code} - #{event.name}"
+                                 })
+      return event      
+    else
+      return e
+    end
+    
+  end
+  
+  def add_data_by_event_type
+    # each event has data titles associated with it
+    case name
+      when "demographics"
+        titles = ["admit date", "suite number", "date of birth", "gender", "ethnic category", "race" ]
+        titles += ["height", "weight", "blood pressure systolic", "blood pressure diastolic", "heart rate"]
+        titles += ["usual school or work bedtime (lower bound)", "usual school or work bedtime (upper bound)"]
+        titles += ["usual day off or weekend bedtime (lower bound)", "usual day off or weekend bedtime (upper bound)"]
+        titles += ["usual school or work waketime (lower bound)", "usual school or work waketime (upper bound)"]
+        titles += ["usual day off or weekend waketime (lower bound)", "usual day off or weekend waketime (upper bound)"]
+        titles += ["desired bedtime", "desired waketime", "naps per week", "nap time"]
+        titles += ["owl lark score"]
+      when "personal data form"
+        titles = ["date of birth", "gender", "ethnic category", "race"]
+      when "physical exam form"
+        titles = ["blood pressure systolic", "blood pressure diastolic", "heart rate"]
+      when "physician form"
+        titles = ["height", "weight", "blood pressure systolic", "blood pressure diastolic", "heart rate"]
+      when "subject information form"
+        titles = ["date of birth", "gender", "height", "weight"]
+        titles += ["usual school or work bedtime (lower bound)", "usual school or work bedtime (upper bound)"]
+        titles += ["usual day off or weekend bedtime (lower bound)", "usual day off or weekend bedtime (upper bound)"]
+        titles += ["usual school or work waketime (lower bound)", "usual school or work waketime (upper bound)"]
+        titles += ["usual day off or weekend waketime (lower bound)", "usual day off or weekend waketime (upper bound)"]
+        titles += ["desired bedtime", "desired waketime", "naps per week", "nap time"]
+      when "owl lark form"
+        titles = ["owl lark score"]
+      when "other sources"
+        titles = ["date of birth", "gender", "ethnic category", "race", "height", "weight"]
+        titles += ["usual school or work bedtime (lower bound)", "usual school or work bedtime (upper bound)"]
+        titles += ["usual day off or weekend bedtime (lower bound)", "usual day off or weekend bedtime (upper bound)"]
+        titles += ["usual school or work waketime (lower bound)", "usual school or work waketime (upper bound)"]
+        titles += ["usual day off or weekend waketime (lower bound)", "usual day off or weekend waketime (upper bound)"]
+        titles += ["desired bedtime", "desired waketime", "naps per week", "nap time"]
+    end
+    titles.each { |title| data.build(:title => title) }
+  end
+  
+  
+  def add_update_data(params)
+    # add or update applicable data fields to event
+    params.each do |title, value|
+      convert_units!(value)
+      title_ns = title.tr('_', ' ')
+      
+      data_attributes = value.merge({ :title => title_ns }) 
+      data_attributes[:missing] ||= 'f'
+
+      if (datum = data.find_by_title(title_ns))
+        datum.update_attributes(data_attributes)
+      else
+        datum = data.build(data_attributes)
+      end
+        
+      # make sure missing ==> no data stored
+      if datum.missing
+        datum.numeric = nil
+        datum.char = nil
+        datum.timepoint = nil
+        datum.data_unit = nil
+        datum.save
+      end
+      
+      # create/update source information - put in data model
+      if Source.create_source?(data_attributes[:source_attributes])
+        CUSTOM_LOGGER.info "Updating/Creating Source Info"
+        if datum.source
+          CUSTOM_LOGGER.info "update current source"
+          data_attributes[:source_attributes].merge({ :user => Authorization.current_user.id })
+          datum.source.update_attributes(data_attributes[:source_attributes])
+        else
+          CUSTOM_LOGGER.info "create new source"
+          s = Source.create(data_attributes[:source_attributes])
+          s.user = Authorization.current_user.id
+          datum.source = s
+        end
+        datum.save
+      else
+        if datum.source
+          if datum.source.data.length <= 1
+            ds = datum.source
+            datum.source = nil
+            datum.save            
+            ds.destroy
+          else
+            datum.source = nil
+          end
+        end
+      end
+    end
+  end
+  
   private
+  
+  # convert to standard units - modifies value
+  def convert_units!(value)
+    # for now: lb ==> kg and ft/in ==> cm
+    case value[:unit_name]
+      when "ft"
+        u = Unit(value[:numeric])
+        u >>= "cm"
+        value[:numeric] = u.abs
+        value[:unit_name] = "cm"
+      when "lb"
+        u = Unit(value[:numeric] + " lb")
+        u >>= "kg"
+        value[:numeric] = u.abs
+        value[:unit_name] = "kg"
+    end
+  end
   
   def save_data
     self.data.each {|d| d.save}
--- study.rb
+++ study.rb
@@ -3,85 +3,11 @@ class Study < ActiveRecord::Base
   set_sequence_name 'id_seq'
   
   # Attributes
-  attr_accessor :pl_tokens, :pi_tokens 
-  attr_accessible :nickname, :new_irb_attributes, :deleted_irb_ids, :pl_tokens, :pi_tokens
+  attr_accessible :nickname, :pi_id
   
   # Associations
-  has_and_belongs_to_many :irbs
   has_many :subjects
-  has_many :personnel_roles, :dependent => :destroy
-  has_many :personnel, :through => :personnel_roles
+  belongs_to :personnel, :foreign_key => "pi_id"
   has_many :events
-  
-  
-  # Scopes
-
-  # Callbacks
-  after_update :save_irbs
-  before_save :save_personnel
-  before_destroy :delete_personnel_associations, :delete_irb_associations
-  
-  # Getters
-  def pis
-    personnel.find(:all, :include => :personnel_roles, :conditions => "role_name = 'PI'")
-  end
-  
-  def pls
-    personnel.find(:all, :include => :personnel_roles, :conditions => "role_name = 'PL'")
-  end
-  
-  # Setters
-  def new_irb_attributes=(irb_attributes)
-    # Either find existing Irb object or create new one, then add to study
-    irb_attributes.each do |attr|
-      unless (attr[:protocol_id].empty? || attr[:protocol_id].nil?)
-        new_irb = Irb.find_or_create_by_protocol_id(attr[:protocol_id]) 
-        irbs << new_irb unless irbs.include?(new_irb)
-      end
-    end
-  end
-  
-  def deleted_irb_ids=(irb_ids)
-    irb_ids = irb_ids.map(&:to_i)
-    irbs_to_delete = irbs.find_all_by_irb_id(irb_ids)
-    irbs.delete(irbs_to_delete)
-  end
-  
-  # Helpers
-  def save_personnel
-    # adds personnel with their proper roles (either PI or PL) to the study using tokeninput
-    personnel.clear
-    personnel_roles.clear
-    
-    CUSTOM_LOGGER.info pi_tokens
-    CUSTOM_LOGGER.info pl_tokens
-    
-    
-    pi_ids = pi_tokens.split(',')
-    pl_ids = pl_tokens.split(',')
-    ids_roles = pi_ids.map{|pi| Hash[:id => pi, :role => "PI"]}
-    ids_roles.concat( pl_ids.map{|pi| Hash[:id => pi, :role => "PL"]}) 
-    
-    CUSTOM_LOGGER.info ids_roles
-    ids_roles.each do |ir|
-      personnel_roles.build(:personnel_id => ir[:id], :role_name => ir[:role]) 
-    end
-  end
-  
-  def save_irbs
-    irbs.each do |irb|
-      irb.save(false)
-    end
-  end
-  
-  
-  # make sure association table entries are deleted before the user is deleted.  
-  # this enforces oracle foreign key constraints
-  def delete_personnel_associations
-    self.personnel.delete(self.personnel)
-  end
-  def delete_irb_associations
-    self.irbs.delete(self.irbs)
-  end
   
 end
